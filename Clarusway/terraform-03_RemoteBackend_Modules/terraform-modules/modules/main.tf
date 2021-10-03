3562c9aa017144b681eaaa35b1c059401414fa77
hamidgokce <hamidgokce@gmail.com>
2021-10-04 01:56:31 +0300

    modules

--- Clarusway/terraform-03_RemoteBackend_Modules/terraform-modules/modules/main.tf
+++ Clarusway/terraform-03_RemoteBackend_Modules/terraform-modules/modules/main.tf
@@ -1,25 +1,26 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
+
 resource "aws_vpc" "module_vpc" {
-    cidr_block = 
-    tags = {
-      "key" = "value"
-    }
-  
+  cidr_block = var.vpc_cidr_block
+  tags = {
+      Name = "terraform-vpc-${var.env}"
+  }
 }
 
 resource "aws_subnet" "public_subnet" {
-    cidr_block = 
-    vpc_id =  
-    tags = 
-  
+  cidr_block =var.public_subnet_cidr
+  vpc_id = aws_vpc.module_vpc.id
+  tags = {
+      Name = "terraform-public-subnet-${var.env}"
+  }
 }
 
-
 resource "aws_subnet" "private_subnet" {
-    cidr_block = 
-    vpc_id =  
-    tags = 
-  
+  cidr_block = var.private_subnet_cidr
+  vpc_id = aws_vpc.module_vpc.id
+  tags = {
+      Name = "terraform-private-subnet-${var.env}"
+  }
 }
